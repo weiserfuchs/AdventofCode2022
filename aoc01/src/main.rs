59e7c0adceb4eb6551f40e8ec663bd3269813b25
weiserfuchs <weiserfuchs@gmx.net>
2022-12-01 23:12:20 +0100

    d

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -2,8 +2,8 @@ mod aoc01_1;
 mod aoc01_2;
 
 fn main() {
-    print!("aoc01_1");
+    println!("aoc01_1");
     aoc01_1::main();
-    print!("aoc01_2");
+    println!("aoc01_2");
     aoc01_2::main();
 }
